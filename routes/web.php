fcfb60b75d375046bc54c10708a8a1eefe4bca48
arsaaja <arskenzbin@gmail.com>
2026-04-12 00:43:21 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\RelayController;
 
+// halaman utama (view monitoring)
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard'); // nama file blade kamu
 });
+
