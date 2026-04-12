44e469aba419c01a3bffc1860c2539cf42fe50a0
Nashhhh17 <naalvyy@student.ub.ac.id>
2026-04-12 14:59:30 +0700

    jjj

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 // halaman utama (view monitoring)
 Route::get('/', function () {
-    return view('dashboard'); // nama file blade kamu
+    return view('dashboard'); 
 });
 
 // Halaman baru untuk riwayat
