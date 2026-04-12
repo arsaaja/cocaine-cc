02d129d11fdf455d9e04c5040cd42f6b99bc3107
arsaaja <arskenzbin@gmail.com>
2026-04-12 16:56:33 +0800

    backend route

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Device extends Model
 {
-    //
+    protected $fillable = [
+        'device_name',
+        'api_key',
+        'status', // online/offline
+        'last_seen'
+    ];
+
+    // Relasi ke data sensor
+    public function sensorData()
+    {
+        return $this->hasMany(SensorData::class);
+    }
+
+    // Relasi ke antrean perintah
+    public function commands()
+    {
+        return $this->hasMany(Command::class);
+    }
+
+    // Relasi ke log aktivitas
+    public function activityLogs()
+    {
+        return $this->hasMany(ActivityLog::class);
+    }
 }
--- app/Models/WorkerStatus.php
+++ app/Models/WorkerStatus.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class WorkerStatus extends Model
 {
-    //
+    protected $table = 'worker_status';
+
+    protected $fillable = [
+        'device_id',
+        'worker_name',
+        'status',
+        'last_run'
+    ];
+
+    public function device()
+    {
+        return $this->belongsTo(Device::class);
+    }
 }
