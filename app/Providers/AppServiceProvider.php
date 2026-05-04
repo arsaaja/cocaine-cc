b89b8fec5a71bc76f40e03e61fa0402aaada766b
arsaaja <arskenzbin@gmail.com>
2026-05-04 13:37:09 +0800

    EROR  dikit jir

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\SensorData;
 use App\Observers\SensorDataObserver;
 class AppServiceProvider extends ServiceProvider
 {
